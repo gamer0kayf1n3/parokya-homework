4756ed5c67d525e9003319009a68de33719f9e7f
gamer0kayf1n3 <muser0943@gmail.com>
2024-08-17 14:47:29 +0800

    pdf conversion

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
 import notion_grabber
+from threading import Thread
 app = Flask(__name__)
+
+def save_pdf():
+  from playwright.sync_api import sync_playwright
+
+  with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.emulate_media(media="print")
+    page.goto("http://localhost:8080")
+    page.pdf(path=".pdf", margin={"top": "0.25in", "bottom": "0.25in", "left": "0.25in", "right": "0.25in"})
+    browser.close()
 
 @app.route("/")
 def main():
@@ -9,4 +21,6 @@ def main():
   print(worklistData)
   return render_template("index.html", worklistData=worklistData)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+t = Thread(target=save_pdf)
+t.start()
 app.run(host="0.0.0.0", port=8080, debug=True)
