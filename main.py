11a48782ecfc8876eb1fa6100a9f16439cd5fcfb
gamer0kayf1n3 <muser0943@gmail.com>
2024-08-22 22:17:57 +0800

    headless

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def save_pdf():
   from playwright.sync_api import sync_playwright
 
   with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     page.emulate_media(media="print")
     page.goto("http://localhost:8080")
