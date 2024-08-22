6fda4eedebbd6d702b6771d04bd303cacaa49824
gamer0kayf1n3 <muser0943@gmail.com>
2024-08-22 20:53:03 +0800

    android testing

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 import notion_grabber
 from threading import Thread
+import sys
+import os
+import time
+import shutil
+
 app = Flask(__name__)
 
 def save_pdf():
+  # pc method
   from playwright.sync_api import sync_playwright
 
   with sync_playwright() as p:
@@ -14,13 +20,33 @@ def save_pdf():
     page.pdf(path=".pdf", margin={"top": "0.25in", "bottom": "0.25in", "left": "0.25in", "right": "0.25in"})
     browser.close()
 
+def watch_for_pdf():
+  # android method
+  while True:
+    for x in os.listdir("~/storage/downloads"):
+      if x == "Today's Activities.pdf":
+        file = os.path.join("~/storage/downloads", "Today's Activities.pdf")
+        shutil.copy2(file, ".", follow_symlinks=True)
+        os.remove(file)
+        break
+    time.sleep(1)
+
 @app.route("/")
 def main():
   worklist = notion_grabber.Worklist()
   worklistData = worklist.fetch()
   print(worklistData)
   return render_template("index.html", worklistData=worklistData)
+
+
+is_android: bool = hasattr(sys, 'getandroidapilevel')
+if is_android: # i host this script on termux too
+  t = Thread(target=watch_for_pdf)
+  t.start()
+else:
+  t = Thread(target=save_pdf)
+  t.start()
+
+
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-t = Thread(target=save_pdf)
-t.start()
 app.run(host="0.0.0.0", port=8080, debug=True)
